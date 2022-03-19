d90a3e24221f4019bb7932ac807dfd4fb07c87e4
arturdziedziczak0@gmail.com <arturdziedziczak0@gmail.com>
2022-03-19 01:15:32 +0100

    Try to add full page transition only for some pages

--- components/Layout/Transition.js
+++ components/Layout/Transition.js
@@ -1,82 +1,59 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import {
   TransitionGroup,
   Transition as ReactTransition,
 } from "react-transition-group"
 import {gsap} from 'gsap/dist/gsap';
+import Image from "next/image";
+import imagePlanetExpress from '../../public/images/planet-express.png';
 
 export default function Transition({ children, location }) {
   const fullPageTransitionRef = useRef();
 
   const onEnterHandler = (node) => {
-    console.log('onEnter', node);
-    // gsap.killTweensOf(node);
-    // Set initial position and styles
     gsap.set(node, {
       position: "relative"
     });
-    // gsap.set(parentNode.current, { overflow: "hidden" })
-    // Create the animation for the incoming component
-    // gsap.to(node, {
-    //   duration: 0.4, autoAlpha: 1,
-    //   x: 0,
-    //   onComplete: completeCall,
-    //   onCompleteParams: [node, parentNode.current]
-    // });
+    if (location === '/') {
+      console.log('onEnter', node);
+    }
   };
   const onEnteringHandler = (node) => {
-    console.log('onEntering', node);
-    // gsap.killTweensOf(node);
-    // Set initial position and styles
     gsap.set(node, {
       position: "absolute"
     });
-    gsap.to(fullPageTransitionRef.current, {
-      width: '100vw',
-      duration: 1,
-      onComplete: () => {
-	gsap.to(fullPageTransitionRef.current, {
-	  width: '0',
-	  duration: 1,
-	});
-      }
-    });
-    // gsap.set(parentNode.current, { overflow: "hidden" })
-    // Create the animation for the incoming component
-    // gsap.to(node, {
-    //   duration: 0.4, autoAlpha: 1,
-    //   x: 0,
-    //   onComplete: completeCall,
-    //   onCompleteParams: [node, parentNode.current]
-    // });
+    if (location === '/') {
+      gsap.to(fullPageTransitionRef.current, {
+	opacity: 1,
+	zIndex: 10,
+	duration: 1,
+	onComplete: () => {
+	  gsap.to(fullPageTransitionRef.current, {
+	    opacity: '0',
+	    delay: 0.25,
+	    duration: 1.5,
+	    onComplete: () => {
+	      gsap.to(fullPageTransitionRef.current, {
+		zIndex: -1
+	      });
+	    }
+	  });
+	}
+      });
+    }
   };
-
-  const onExitHandler = (node) => {
-    console.log('onExit', node);
-    // gsap.killTweensOf(node);
-    // Set initial position and styles
-    // gsap.set(node, {
-    //   position: "absolute", left: 0
-    // });
-    // // Create the animation for the incoming component
-    // gsap.to(node, {
-    //   duration: 0.4, autoAlpha: 0,
-    //   x: -100
-    // });
-  };
-
-
-  console.log(location);
 
   return (
     <TransitionGroup>
-      <div style={{position: 'fixed', height: '100vh', zIndex: 10, background: 'red' }} ref={fullPageTransitionRef}></div>
+      <div style={{position: 'fixed', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', zIndex: -1, opacity: 0, width: '100vw', background: '#154A44' }} ref={fullPageTransitionRef}>
+	<div style={{width: '300px', height: '300px'}}>
+	  <Image src={imagePlanetExpress} alt="Fry na kolorowym tle" />
+	</div>
+      </div>
       <ReactTransition
 	key={location}
 	onEntering={onEnteringHandler}
 	onEnter={onEnterHandler}
-	onExit={onExitHandler}
-
 	timeout={1000}
       >
 	{children}
