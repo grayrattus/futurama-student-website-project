d67a7eebcc4961c0ffb2769a9c368ccc26542781
arturdziedziczak0@gmail.com <arturdziedziczak0@gmail.com>
2022-03-29 21:54:39 +0200

    Fix full page transition

--- components/Layout/Transition.js
+++ components/Layout/Transition.js
@@ -7,46 +7,46 @@ import {gsap} from 'gsap/dist/gsap';
 import Image from "next/image";
 import imagePlanetExpress from '../../public/images/planet-express.png';
 
+import styles from './Transition.module.css'
+
 export default function Transition({ children, menuButtonRef, location }) {
   const fullPageTransitionRef = useRef();
 
   const onEnteringHandler = (node) => {
     menuButtonRef.current.checked = false;
-    gsap.set(node, {
-      position: "absolute"
-    });
     gsap.to(fullPageTransitionRef.current, {
-      opacity: 1,
-      zIndex: 10,
-      duration: 1.4,
+      opacity: '0',
+      delay: 1.5,
+      duration: 2,
       onComplete: () => {
-	gsap.set(node, {
-	  position: "relative"
-	});
 	gsap.to(fullPageTransitionRef.current, {
-	  opacity: '0',
-	  delay: 0.5,
-	  duration: 1.5,
-	  onComplete: () => {
-	    gsap.to(fullPageTransitionRef.current, {
-	      zIndex: 0
-	    });
-	  }
+	  zIndex: 0
 	});
       }
     });
   };
 
+  const onExitHandler = (node) => {
+    gsap.to(fullPageTransitionRef.current, {
+      zIndex: 10
+    })
+    gsap.to(fullPageTransitionRef.current, {
+      opacity: 1,
+      duration: 0.2,
+    });
+  }
+
   return (
     <TransitionGroup>
-      <div style={{position: 'fixed', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', zIndex: -1, opacity: 0, width: '100vw', background: '#154A44' }} ref={fullPageTransitionRef}>
-	<div style={{width: '300px', height: '300px'}}>
-	  <Image src={imagePlanetExpress} alt="Fry na kolorowym tle" />
+      <div className={styles.transition} ref={fullPageTransitionRef}>
+	<div className={styles.transitionBackground}>
+	  <Image src={imagePlanetExpress} alt="Logo planet express" />
 	</div>
       </div>
       <ReactTransition
 	key={location}
 	onEntering={onEnteringHandler}
+	onExit={onExitHandler}
 	timeout={1500}
       >
 	{children}
